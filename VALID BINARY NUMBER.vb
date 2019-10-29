5562018d3cb9a22324b8c351979e8a2267d68214
hanzala2203 <55300056+hanzala2203@users.noreply.github.com>
2019-10-29 11:37:29 -0700

    Add files via upload

---
+++ VALID BINARY NUMBER.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Mystr As String
+        Dim counter As Integer
+        Dim validity As Boolean
+        Dim Mychar As String
+
+        Mystr = " "
+        counter = 0
+        validity = True
+        Mychar = " "
+
+        Console.Write("Enter Binary Number: ")
+        mystr = Console.ReadLine
+
+        If Len(Mystr) < 1 Or Len(Mystr) > 8 Then
+            validity = False
+        End If
+        If validity = True Then
+            For counter = 1 To Len(mystr)
+                Mychar = Mid(Mystr, counter, 1)
+                If mychar < 0 Or mychar > 1 Then
+                    validity = False
+                End If
+            Next
+
+        End If
+
+        If validity = True Then
+            Console.WriteLine("The Number Is Binary")
+            Console.ReadKey()
+        ElseIf validity = False Then
+            Console.WriteLine("The Number Is Not Binary")
+            Console.ReadKey()
+        End If
+        Console.ReadKey()
+    End Sub
+
+End Module
